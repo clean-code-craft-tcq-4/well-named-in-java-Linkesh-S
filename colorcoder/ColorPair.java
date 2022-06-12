4a1d607b423f2ab73d7fd80fdcb8b4c13e762bcd
Linkesh S <linkesh.s.1111@gmail.com>
2022-06-12 18:23:10 +0530

    Create ColorPair.java

---
+++ colorcoder/ColorPair.java
@@ -0,0 +1,24 @@
+package colorcoder;
+
+    static class ColorPair {
+        private MajorColor majorColor;
+        private MinorColor minorColor;
+        
+        public ColorPair(MajorColor major, MinorColor minor)
+        {
+            majorColor = major;
+            minorColor = minor;
+        }
+        public MajorColor getMajor() {
+            return majorColor;
+        }
+        public MinorColor getMinor() {
+            return minorColor;
+        }
+        String ToString() {
+            String colorPairStr = MajorColorNames[majorColor.getIndex()];
+            colorPairStr += " ";
+            colorPairStr += MinorColorNames[minorColor.getIndex()];
+            return colorPairStr;
+        }
+    };
