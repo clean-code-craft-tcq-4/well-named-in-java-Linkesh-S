a6220e8b04f3c3bdbee45844b23bf995b863e238
Linkesh S <linkesh.s.1111@gmail.com>
2022-06-13 21:57:12 +0530

    Update MajorColor.java

--- colorcoder/MajorColor.java
+++ colorcoder/MajorColor.java
@@ -1,18 +1,24 @@
 package colorcoder;
 
-enum MajorColor implements Color{
+enum MajorColor {
         WHITE(0),
         RED(1),
         BLACK(2),
         YELLOW(3),
         VIOLET(4);
-  
-        @Override
-        int getIndex() {
-            return index;
-        }
+        private int index;
         private MajorColor(int index) {
             this.index = index;
         }
-
+        int getIndex() {
+            return index;
+        }
+        public static MajorColor fromIndex(int index) {
+            for(MajorColor color: MajorColor.values()) {
+                if(color.getIndex() == index) {
+                    return color;
+                }
+            }
+            return null;
+        }
     };
