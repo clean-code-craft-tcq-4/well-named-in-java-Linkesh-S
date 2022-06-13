209449946f312c741bc74213df7f533594b9181c
Linkesh S <linkesh.s.1111@gmail.com>
2022-06-13 21:45:04 +0530

    Update Color.java

--- colorcoder/Color.java
+++ colorcoder/Color.java
@@ -1,7 +1,11 @@
 package colorcoder;
 
-public interface enum Color{
+public interface Color{
 
+        int index=0;
+        int getIndex() {
+            return index;
+        }
         public static <E extends Enum<E>& Color>E fromIndex(int index) {
             for(E color: E.values()) {
                 if(color.getIndex() == index) {
