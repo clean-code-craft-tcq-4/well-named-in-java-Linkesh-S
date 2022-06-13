ec45f1a94ee0c25686a2d169b52a17c34139870e
Linkesh S <linkesh.s.1111@gmail.com>
2022-06-13 20:34:46 +0530

    Update Main.java

--- colorcoder/Main.java
+++ colorcoder/Main.java
@@ -24,7 +24,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Test test;
+        Test test= new Test();
        test.testNumberToPair(4, MajorColor.WHITE, MinorColor.BROWN);
         test.testNumberToPair(5, MajorColor.WHITE, MinorColor.SLATE);
     
